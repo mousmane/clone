b8619cbf2a5036f5f7432977706953c419553033
Pascal Cantat <pcantat@gmail.com>
2018-05-18 12:09:24 +0200

    gestion conflit Lorem30...

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
     </head>
     <body>
     <p>Hello!</p>
+    <i>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae sint iste ipsam dolor vel voluptatem facere officiis ut, omnis pariatur dolorem ex, necessitatibus, corrupti inventore laudantium voluptas dicta natus repudiandae?</i>
     </body>
 </html>
