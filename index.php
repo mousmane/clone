9adaa3cbad7b1daa992bf4ad3041947bf680762e
Ousmane MAMA <mousmane@hotmail.com>
2018-05-18 11:59:40 +0200

    modif contenu paragraphe en lorem5

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
         <meta charset="UTF-8">
     </head>
     <body>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dignissimos ea nobis fugit minima! Omnis, consectetur numquam! Quia atque voluptatem non vitae expedita excepturi praesentium ipsum? Quod vitae quas consequatur repellendus temporibus eveniet suscipit maiores tempore magni natus voluptate architecto facilis officiis blanditiis qui similique, aliquam magnam tenetur, provident saepe sequi recusandae officia cum illo. Repellat quae distinctio voluptates repudiandae doloremque quaerat, aspernatur pariatur? Amet officia possimus voluptatem dolorem aliquam iure corrupti suscipit non distinctio?</p>
+    <p>Lorem ipsum dolor sit amet.</p>
     </body>
 </html>
